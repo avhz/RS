8e88d98ad045d303ce2cb501889fa9d2aedca674
avhz <RustQuantContact@gmail.com>
2025-06-07 16:16:14 +0200

    docs: update

--- R/class.R
+++ R/class.R
@@ -26,11 +26,17 @@ Class <- function(.classname, ..., .validate = TRUE) {
     #     return(.m)
     # }
     # .methods <- lapply(list(...), .method)
+    .attrs <- rlang::list2(...)
+
+    .fields <- lapply(
+        .attrs,
+        function(.a) if (inherits(attr, "ClassType")) return(.a)
+    )
 
     .self <- .Call(
         "wrap__ClassDefinition__new",
         name = .classname,
-        methods = rlang::list2(...),
+        methods = .attrs,
         validate = .validate,
         PACKAGE = "RS"
     )
@@ -44,8 +50,17 @@ Class <- function(.classname, ..., .validate = TRUE) {
         )
     }
 
+    ## Handle un-named arguments
+    ## Need to handle 'fields' argument in wrap__ClassInstance__new
+
+    # formals(new_class) <- do.call(
+    #     alist,
+    #     setNames(rep(list(quote(expr = )), length(.fields)), names(.fields))
+    # )
+
     assign(.classname, new_class, envir = parent.frame())
 }
+
 
 ## ============================================================================
 ## Decorators
@@ -146,4 +161,116 @@ print.extendr_error <- function(error, ...) {
         return(.Call("wrap__class_equality", cls1, cls2, PACKAGE = "RS"))
     }
     stop("Both arguments must be `ClassInstance` objects.")
+}
+
+## ============================================================================
+## PLAYGROUND
+## ============================================================================
+
+if (FALSE) {
+    . <- function() {
+        gc()
+        remove(list = ls())
+        # rextendr::clean()
+        rextendr::document()
+        devtools::load_all()
+        devtools::test()
+    }
+    .()
+
+    (bm <- .benchmark(1e4))
+    .benchplot(bm)
+    ggplot2::autoplot(bm)
+
+    # png("benchmark.png", width = 800, height = 600)
+    # .benchplot(bm)
+    # dev.off()
+
+    Class(
+        "Foo",
+        a = t_int,
+        b = private(t_dbl),
+
+        ## Methods
+        bar = private(function(self, x) {
+            self@a + x * self@b
+        }),
+        qux = function(self) {
+            print(class(self))
+        },
+
+        ## Static methods
+        baz = static(function(self, x) {
+            self + x
+        })
+    )
+
+    bench::mark(Foo(a = 1L), iterations = 1e5)
+
+    foo <- Foo(a = 1L, b = 2.0)
+    foo@qux()
+    foo@a
+    foo@b
+    foo@bar(2L)
+    foo@baz(2L, 3L)
+
+    Class("Foo", a = t_int, b = t_dbl)
+    Class("Bar", foo = Foo)
+
+    foo <- Foo(a = 1L, b = 2.0)
+    foo <- Foo(1L, 2.0)
+    bar <- Bar(foo = 1)
+
+    foo@a
+    bar@foo@a
+    bar@foo@b
+
+    ## Define class
+    Class(
+        "FooParallel",
+        a = t_int,
+
+        ## Methods
+        bar = function(self, x) self@a + x,
+    )
+
+    foo <- FooParallel(a = 1L)
+    foo@a
+    foo@bar(2L)
+
+    ## Create classes in parallel via Mirai
+    mirai::daemons(0L)
+    cfg <- mirai::serial_config(
+        c("ClassDefinition", "ClassInstance"),
+        list(function(x) serialize(x, NULL), function(x) serialize(x, NULL)),
+        list(base::unserialize, base::unserialize)
+    )
+
+    mirai::daemons(4L, dispatcher = FALSE, serial = cfg)
+    e <- mirai::everywhere({
+        devtools::load_all()
+        Class(
+            "FooParallel",
+            a = t_int,
+
+            ## Methods
+            bar = function(self, x) self@a + x,
+        )
+    })
+
+    m <- mirai::mirai(
+        {
+            FooParallel(a = 1L)
+        }
+        # FooParallel = FooParallel
+    )[]
+
+    m
+    (foos <- mirai::mirai_map(
+        1:3,
+        \(i) FooParallel(a = as.integer(i)),
+        # FooParallel = FooParallel
+    )[])
+
+    foos[1]$data
 }
